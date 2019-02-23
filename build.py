2438dcdcb97cda1393f5e6212dc7a2423048801d
Simon Pinfold <simon@uint8.me>
2019-02-23 16:43:51 +1300

    Add CI tests

--- build.py
+++ build.py
@@ -2,7 +2,17 @@ import os
 import zipfile
 import boto3
 
-from overtrack.util import humansize
+
+def humansize(nbytes, suffixes=('B', 'KB', 'MB', 'GB', 'TB', 'PB')):
+    # http://stackoverflow.com/a/14996816
+    if nbytes == 0:
+        return '0 B'
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes) - 1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
 
 
 def main():
@@ -13,6 +23,7 @@ def main():
         'ingest',
         'twitch_overlay',
 
+        'pyproject.toml',
         'requirements.txt'
     ]
     blacklist = [
