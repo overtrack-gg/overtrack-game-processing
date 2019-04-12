745151ed0eb0d522da80d9ed78dd0c398037a0e3
Simon Pinfold <simon@uint8.me>
2019-04-13 00:10:45 +1200

    Improve placements for when match summary != squad summary
    [ci skip]

--- overtrack/apex/collect/apex_game.py
+++ overtrack/apex/collect/apex_game.py
@@ -1093,7 +1093,9 @@ class ApexGame:
 
         if match_summary_placed and squad_summary_placed:
             if match_summary_placed != squad_summary_placed:
-                logger.error(f'Got match summary > placed: {match_summary_placed} != squad summary > placed: {squad_summary_placed}')
+                logger.warning(f'Got match summary > placed: {match_summary_placed} != squad summary > placed: {squad_summary_placed} - using match summary')
+                logger.error(f'Got match summary > placed != squad summary > placed', exc_info=True)
+                return match_summary_placed
 
         if match_summary_placed:
             logger.info(f'Using placed from match summary: {match_summary_placed}')
