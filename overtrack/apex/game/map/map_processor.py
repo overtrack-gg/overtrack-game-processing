ee295ae9108dffb4a1416bb6d61fbae7bc1f73ef
Simon Pinfold <simon@uint8.me>
2019-04-19 01:24:23 +1200

    Support rotating map

--- overtrack/apex/game/map/map_processor.py
+++ overtrack/apex/game/map/map_processor.py
@@ -1,23 +1,25 @@
 import logging
 import os
-from typing import List, NamedTuple, Optional
+from collections import deque
 
 import cv2
 import numpy as np
 
+from overtrack.apex import ocr
 from overtrack.frame import Frame
 from overtrack.processor import Processor
-from overtrack.util import imageops, time_processing, ts2s
+from overtrack.util import imageops, time_processing
 from overtrack.util.logging_config import config_logger
 from overtrack.util.region_extraction import ExtractionRegionsCollection
 from .models import *
 
+logger = logging.getLogger('MapProcessor')
 
 def _draw_map_location(
         debug_image: Optional[np.ndarray],
         map_image: np.ndarray,
         location: Location,
-        min_loc: Tuple[int, int],
+        min_loc: Optional[Tuple[int, int]],
         shape: Tuple[int, int]) -> None:
     if debug_image is None:
         return
@@ -38,13 +40,14 @@ def _draw_map_location(
         (0, 255, 0),
         2
     )
-    cv2.rectangle(
-        out,
-        min_loc,
-        (min_loc[0] + shape[1], min_loc[1] + shape[0]),
-        (0, 0, 255),
-        4
-    )
+    if min_loc:
+        cv2.rectangle(
+            out,
+            min_loc,
+            (min_loc[0] + shape[1], min_loc[1] + shape[0]),
+            (0, 0, 255),
+            4
+        )
     out = cv2.resize(out, (0, 0), fx=0.25, fy=0.25)
     debug_image[100:100 + out.shape[0], 400:400 + out.shape[1]] = out
 
@@ -69,6 +72,9 @@ class MapProcessor(Processor):
     MAP_MASK = imageops.imread(os.path.join(os.path.dirname(__file__), 'data', 'map_mask.png'), 0) == 255
     TEMPLATE_OFFSET = (0, -3)
 
+    def __init__(self):
+        self.map_rotated = deque(maxlen=10)
+
     def eager_load(self):
         self.REGIONS.eager_load()
 
@@ -80,104 +86,147 @@ class MapProcessor(Processor):
         map_image = cv2.Canny(map_image, 30, 100)
         map_image = cv2.GaussianBlur(map_image, (0, 0), 1.5)
 
-        template = cv2.resize(
+        map_fragment = cv2.resize(
             map_image,
             (0, 0),
             fx=0.987,
             fy=0.987
         )
-        match = cv2.matchTemplate(
-            self.MAP_TEMPLATE,
-            template,
-            cv2.TM_SQDIFF_NORMED
+
+        if len(self.map_rotated) and sum(self.map_rotated) / len(self.map_rotated) > 0.75:
+            # try rotated map first
+            rotated_map = True
+            bearing, location, min_loc, min_val = self._get_location_rotated(frame, map_fragment)
+            if not bearing or min_val > 0.9:
+                location2, min_loc2, min_val2 = self._get_location(map_fragment)
+                if not bearing or min_val2 < min_val:
+                    rotated_map = False
+                    location, min_loc, min_val = location2, min_loc2, min_val2
+        else:
+            # by default try without rotating map
+            rotated_map = False
+            location, min_loc, min_val = self._get_location(map_fragment)
+            if min_val > 0.9:
+                bearing, location2, min_loc2, min_val2 = self._get_location_rotated(frame, map_fragment)
+                if bearing and min_val2 < min_val:
+                    rotated_map = True
+                    location, min_loc, min_val = location2, min_loc2, min_val2
+            else:
+                bearing = None
+
+        _draw_map_location(
+            frame.debug_image,
+            self.MAP,
+            location,
+            min_loc if not rotated_map else None,
+            map_fragment.shape
         )
 
+        if min_val < 0.85 and self.MAP_MASK[location.coordinates[1], location.coordinates[0]]:
+            self.REGIONS.draw(frame.debug_image)
+
+            self.map_rotated.append(rotated_map)
+            frame.location = location
+            frame.location.bearing = bearing
+            frame.location.rotated_map = rotated_map
+            return min_val < 0.6
+
+        return False
+
+    def _get_location_rotated(self, frame, map_fragment):
+        bearing_image = self.REGIONS['bearing'].extract_one(frame.image_yuv[:, :, 0])
+        bearing = imageops.tesser_ocr(bearing_image, expected_type=int, engine=ocr.tesseract_ttlakes_digits)
+        if bearing:
+            rot = cv2.getRotationMatrix2D(
+                (self.MAP_TEMPLATE.shape[1] // 2, self.MAP_TEMPLATE.shape[0] // 2),
+                bearing - 360,
+                1
+            )
+            logger.debug(f'Got bearing={bearing}')
+            location2, min_loc2, min_val2 = self._get_location(map_fragment, rotation=rot)
+            return bearing, location2, min_loc2, min_val2
+        else:
+            return None, None, None, 1
+
+    def _get_location(self, region: np.ndarray, rotation=None):
+        if rotation is None:
+            map_template = self.MAP_TEMPLATE
+        else:
+            height, width = self.MAP_TEMPLATE.shape
+            map_template = cv2.warpAffine(
+                self.MAP_TEMPLATE,
+                rotation,
+                (width, height)
+            )
+
+        match = cv2.matchTemplate(
+            map_template,
+            region,
+            cv2.TM_SQDIFF_NORMED
+        )
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
-        coords = min_loc[0] + template.shape[1] // 2 + self.TEMPLATE_OFFSET[0], min_loc[1] + template.shape[0] // 2 + self.TEMPLATE_OFFSET[1]
+
+        # d = cv2.cvtColor(map_template, cv2.COLOR_GRAY2BGR)
+        # cv2.rectangle(d, min_loc, (min_loc[0] + region.shape[1], min_loc[1] + region.shape[0]), (0, 255, 0))
+        # cv2.circle(
+        #     d,
+        #     (min_loc[0] + region.shape[1] // 2 + self.TEMPLATE_OFFSET[0], min_loc[1] + region.shape[0] // 2 + self.TEMPLATE_OFFSET[1]),
+        #     3,
+        #     (0, 0, 255),
+        #     -1
+        # )
+        # cv2.imshow('template_rot', d)
+
+        coords = min_loc[0] + region.shape[1] // 2 + self.TEMPLATE_OFFSET[0], min_loc[1] + region.shape[0] // 2 + self.TEMPLATE_OFFSET[1]
+        if rotation is not None:
+            inv = cv2.invertAffineTransform(rotation)
+            coords = tuple(cv2.transform(np.array([[coords]]), inv)[0][0])
+
+        # d2 = cv2.cvtColor(self.MAP_TEMPLATE, cv2.COLOR_GRAY2BGR)
+        # cv2.circle(
+        #     d2,
+        #     coords,
+        #     3,
+        #     (0, 0, 255),
+        #     -1
+        # )
+        # cv2.imshow('template', d2)
 
         location = Location(
             coords,
             round(min_val, 5)
         )
-        _draw_map_location(
-            frame.debug_image,
-            self.MAP,
-            location,
-            min_loc,
-            template.shape
-        )
-
-        if min_val < 0.85 and self.MAP_MASK[location.coordinates[1], location.coordinates[0]]:
-            self.REGIONS.draw(frame.debug_image)
-            frame.location = location
-            return min_val < 0.6
-
-        return False
+        return location, min_loc, min_val
 
 
 def main() -> None:
-    frame = Frame.create(
-        cv2.imread(
-            "C:/Users/simon/mpv-screenshots/mpv-shot0209.png"
-        ),
-        0,
-        True
-    )
-    MapProcessor().process(
-        frame
-    )
-    cv2.imshow('debug', frame.debug_image)
-    frame.strip()
-    print(frame)
-    cv2.waitKey(0)
+    import glob
+
     config_logger('map_processor', logging.INFO, write_to_file=False)
 
     pipeline = MapProcessor()
-    path: List[Tuple[int, int]] = []
-
-    from overtrack.source.stream import Twitch
-
-    source = Twitch(
-        'https://www.twitch.tv/videos/387748639',
-        2,
-        keyframes_only=False,
-        seek=ts2s('5:54:30'),
-        debug_frames=True
-    )
-    f = True
-    while True:
-        frame = source.get()
-        if frame is None:
-            break
-
-        if pipeline.process(frame):
-            location = frame.location
-            path.append(location.coordinates)
-
+    print(os.path.abspath('../../../../dev/images/map/'))
+    for p in glob.glob('../../../../dev/images/map/*.png'):
+        print(p)
+        try:
+            frame = Frame.create(
+                cv2.resize(cv2.imread(p), (1920, 1080)),
+                0,
+                True
+            )
+        except:
+            continue
+        pipeline.process(frame)
+        print(frame)
+        pipeline.REGIONS.draw(frame.debug_image)
         cv2.imshow('debug', frame.debug_image)
 
-        map = pipeline.MAP.copy()
-        if len(path):
-            prev = path[0]
-            for p in path:
-                cv2.line(
-                    map,
-                    prev,
-                    p,
-                    (0, 255, 0),
-                    1
-                )
-                prev = p
-        cv2.imshow(
-            'map',
-            cv2.resize(map, (0, 0), fx=0.8, fy=0.8)
-        )
 
-        # if f:
-        #     cv2.waitKey(0)
-        #     f = False
 
-        cv2.waitKey(1)
+        if 'match_status' in frame and frame.match_status.kills:
+            cv2.waitKey(0)
+        else:
+            cv2.waitKey(0)
 
 
 if __name__ == '__main__':
