c3c26a379ead2dafdbc22bf0bc2f729d112829d2
Simon Pinfold <simon@uint8.me>
2019-11-08 23:18:10 +1300

    Add Scrims season

--- overtrack/apex/data/seasons.py
+++ overtrack/apex/data/seasons.py
@@ -27,13 +27,16 @@ _season_2_start = datetime.datetime.strptime(
     'Jul 2 2019 10:00AM',
     '%b %d %Y %I:%M%p'
 ).replace(tzinfo=_PDT)
+_season_3_start = 1569956446
 
 SEASONS = [
     Season(0, 0, _season_1_start.timestamp()),
     Season(1, _season_1_start.timestamp(), _season_2_start.timestamp()),
-    Season(2, _season_2_start.timestamp(), 1569956446),
-    Season(3, 1569956446, float('inf')),
+    Season(2, _season_2_start.timestamp(), _season_3_start),
+    Season(3, _season_3_start, float('inf')),
 
-    Season(1002, 0, 0, season_name='Season 2 Solo'),
-    Season(1003, 1569956446, float('inf'), season_name='Season 3 Duos'),
+    Season(1002, 0, 0, season_name='Season 2 Solos'),
+    Season(1003, _season_3_start, float('inf'), season_name='Season 3 Duos'),
+
+    Season(2000, _season_3_start, float('inf'), season_name='Scrims'),
 ]
