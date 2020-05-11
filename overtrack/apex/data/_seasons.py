eaeeb29607926880de90cc48183f02b20742974c
Simon Pinfold <simon@uint8.me>
2020-05-12 10:36:50 +1200

    Apex: Add season 5

--- overtrack/apex/data/_seasons.py
+++ overtrack/apex/data/_seasons.py
@@ -34,13 +34,19 @@ _season_4_start = datetime.datetime.strptime(
     'Feb 4 2020 10:00AM',
     '%b %d %Y %I:%M%p'
 ).replace(tzinfo=_PST)
+_season_5_start = datetime.datetime.strptime(
+    # https://twitter.com/ApexLegendNews/status/1257370233628700678
+    'May 12 2020 10:00AM',
+    '%b %d %Y %I:%M%p'
+).replace(tzinfo=_PDT)
 
 seasons = [
     Season(0, 0, _season_1_start.timestamp(), has_ranked=False),
     Season(1, _season_1_start.timestamp(), _season_2_start.timestamp(), has_ranked=False),
     Season(2, _season_2_start.timestamp(), _season_3_start),
     Season(3, _season_3_start, _season_4_start.timestamp()),
-    Season(4, _season_4_start.timestamp(), float('inf')),
+    Season(4, _season_4_start.timestamp(), _season_5_start.timestamp()),
+    Season(5, _season_5_start.timestamp(), float('inf')),
 
     Season(1002, 0, 0, season_name='Season 2 Solos', has_ranked=False),
     Season(1003, _season_3_start, _season_4_start.timestamp(), season_name='Season 3 Duos', has_ranked=False),
@@ -49,3 +55,11 @@ seasons = [
     Season(2000, _season_3_start, float('inf'), season_name='Scrims', has_ranked=False),
 ]
 current_season = sorted([s for s in seasons if s.index < 100], key=lambda s: s.end)[-1]
+
+
+def main():
+    from pprint import pprint
+    pprint(seasons)
+
+if __name__ == '__main__':
+    main()
