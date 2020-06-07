48bc7009fc3e834b5d3e8e26b6682a2976f218b4
Simon Pinfold <simon@uint8.me>
2020-06-07 15:12:37 +1200

    Valorant: Refactor imports

--- overtrack/valorant/collect/valorant_game/__init__.py
+++ overtrack/valorant/collect/valorant_game/__init__.py
@@ -1,8 +1,2 @@
-from overtrack.valorant.collect.valorant_game.clips import Clip
-from overtrack.valorant.collect.valorant_game.invalid_game import InvalidGame
-from overtrack.valorant.collect.valorant_game.invalid_game import InvalidGame
-from overtrack.valorant.collect.valorant_game.kills import Kills, Kill
-from overtrack.valorant.collect.valorant_game.performance_stats import PerformanceStats
-from overtrack.valorant.collect.valorant_game.rounds import Rounds
-from overtrack.valorant.collect.valorant_game.teams import Teams, Player, Ult
 from overtrack.valorant.collect.valorant_game.valorant_game import ValorantGame
+from overtrack.valorant.collect.valorant_game.invalid_game import InvalidGame
